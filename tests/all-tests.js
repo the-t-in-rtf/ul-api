5fec640e9e02d8bfb389d68bdc3d7a022527adc5
Tony Atkins <tony@raisingthefloor.org>
2016-10-18 12:08:23 +0200

    UL-65: Collapsed node/all tests into a single file.

--- tests/all-tests.js
+++ tests/all-tests.js
@@ -1,4 +1,12 @@
 /* eslint-env node */
+// Tests that are run using Node.  We also have tests that run using testem, see `/testem.js` for details.
+//
 "use strict";
-// TODO: Wire up browser tests and combine coverage information (remove upstream tests script in favor of grunt task).
-require("./node-tests");
+
+// TODO:  Review this with Antranig.  From the coverage reports, it looks like the tests are running, but no test output is displayed.
+require("./js/docs-tests");
+require("./js/product");
+require("./js/products-tests");
+require("./js/search-tests");
+require("./js/sources-tests");
+require("./js/updates-tests");
